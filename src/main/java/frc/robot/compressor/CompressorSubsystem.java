01eace269ce36fe5bfb16a14db44305fbe2c7e31
Jorden <120529450+H0wdyW0R1D@users.noreply.github.com>
2023-02-08 17:44:56 -0500

    Compressor Subsystem
    Created the compressor subsystem and added a compressor class to Constants.java

---
+++ src/main/java/frc/robot/compressor/CompressorSubsystem.java
@@ -0,0 +1,22 @@
+package frc.robot.compressor;
+
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
+public class CompressorSubsystem {
+  private Compressor pnCompressor = new Compressor(0, PneumaticsModuleType.REVPH);
+
+  public void run() {
+    // TODO: investigate enableAnalog vs enableDigital
+    pnCompressor.enableAnalog(Constants.Compressor.minPressure, Constants.Compressor.maxPressure);
+  }
+
+  public void stop() {
+    pnCompressor.disable();
+  }
+
+  public boolean isFull() {
+    return pnCompressor.getPressure() > Constants.Compressor.compressorThreshold;
+  }
+}
