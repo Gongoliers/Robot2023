326d8e2ca195eb07f84cabda209ec0682ba9d8b7
natewest68 <122481174+natewest68@users.noreply.github.com>
2023-02-22 15:53:01 -0500

    Changed method names
    In SwerveTelemetry, I changed the method names from their simple form (heading, odometryX, etc.) and added "add" as the prefix.

--- src/main/java/frc/robot/swerve/SwerveTelemetry.java
+++ src/main/java/frc/robot/swerve/SwerveTelemetry.java
@@ -12,32 +12,32 @@ public class SwerveTelemetry {
 
     public static void createShuffleboardTab(Swerve swerve, String tabName) {
         ShuffleboardTab tab = Shuffleboard.getTab(tabName);
-        heading(swerve, tab).withPosition(0, 0);
-        odometryX(swerve, tab).withPosition(0, 1);
-        odometryY(swerve, tab).withPosition(0, 2);
+        addHeading(swerve, tab).withPosition(0, 0);
+        addOdometryX(swerve, tab).withPosition(0, 1);
+        addOdometryY(swerve, tab).withPosition(0, 2);
 
-        module(swerve, tab, 0, "Front Left Module").withPosition(1, 0);
-        module(swerve, tab, 1, "Front Right Module").withPosition(2, 0);
-        module(swerve, tab, 2, "Back Left Module").withPosition(3, 0);
-        module(swerve, tab, 3, "Back Right Module").withPosition(4, 0);
+        addModule(swerve, tab, 0, "Front Left Module").withPosition(1, 0);
+        addModule(swerve, tab, 1, "Front Right Module").withPosition(2, 0);
+        addModule(swerve, tab, 2, "Back Left Module").withPosition(3, 0);
+        addModule(swerve, tab, 3, "Back Right Module").withPosition(4, 0);
     }
 
-    private static SuppliedValueWidget<Double> heading(Swerve swerve, ShuffleboardTab tab) {
+    private static SuppliedValueWidget<Double> addHeading(Swerve swerve, ShuffleboardTab tab) {
         var widget = tab.addNumber("Heading", () -> swerve.pose().getRotation().getDegrees());
         return widget;
     }
         
-    private static SuppliedValueWidget<Double> odometryX(Swerve swerve, ShuffleboardTab tab) {
+    private static SuppliedValueWidget<Double> addOdometryX(Swerve swerve, ShuffleboardTab tab) {
         var widget = tab.addNumber("Odometry X", () -> swerve.pose().getX());
         return widget;
     }
 
-    private static SuppliedValueWidget<Double> odometryY(Swerve swerve, ShuffleboardTab tab) {
+    private static SuppliedValueWidget<Double> addOdometryY(Swerve swerve, ShuffleboardTab tab) {
         var widget = tab.addNumber("Odometry Y", () -> swerve.pose().getY());
         return widget;
     }
 
-    private static ShuffleboardLayout module(Swerve swerve, ShuffleboardTab tab, int moduleNumber, String moduleName) {
+    private static ShuffleboardLayout addModule(Swerve swerve, ShuffleboardTab tab, int moduleNumber, String moduleName) {
         var layout = tab.getLayout(moduleName, BuiltInLayouts.kGrid);
         layout.withProperties(Map.of("Label position", "TOP"));
 
