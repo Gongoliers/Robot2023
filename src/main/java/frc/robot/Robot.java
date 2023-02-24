008b44afadb73db7ef2285be9bd24259147fe2c9
Hayden Heroux <haydenheroux@gmail.com>
2023-02-23 20:48:45 -0500

    feat(Telemetry): Enable DriverStation logging.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -32,6 +34,14 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
+    // Use a DataLogManager to automatically handle the saving / sharing of logging info
+    DataLogManager.start();
+
+    // Log DriverStation data, including joystick data, so specific error conditions can be recreated
+    DriverStation.startDataLog(DataLogManager.getLog());
+    // Disable joystick warnings, since false positives may be distracting
+    DriverStation.silenceJoystickConnectionWarning(true);
   }
 
   /**
