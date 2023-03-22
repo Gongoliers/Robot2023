1654d3f1a32d5ac8c82b969b1a9df26085b833aa
Hayden Heroux <haydenheroux@gmail.com>
2023-03-22 11:04:56 -0400

    fix: Disable compressor in auto and teleop.
    This change prevents the compressor from going on if you transition from test mode to auto or teleop.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -78,6 +78,8 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
+    m_compressor.disable();
+
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
@@ -92,6 +94,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
+    m_compressor.disable();
+
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
